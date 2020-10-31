d214f36cd53f40ecbfb0bc8769fcce05f72665bf
Silviu Iosif <6332838@rts-365.dk>
2020-10-31 17:51:54 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,62 @@
+
+var imageElement = document.querySelector(".image");
+var buttonLeft = document.querySelector('#buttonLeft');
+var buttonRight = document.querySelector('#buttonRight');
+
+function slideShow(image, images, time, mouseEvent = true, clickEvent = false){
+    
+    let run = true;
+    let index = 0;
+    function showImage(imageIndex) {
+        image.src = images[imageIndex];                       
+    }
+
+    image.src = images[0];
+
+    if(mouseEvent){
+        imageElement.addEventListener("mouseenter", function(){
+            run = false;
+        })
+
+        imageElement.addEventListener("mouseleave", function(){
+                run = true;    
+        })
+    }
+
+//button functionalitet----------------------------------------
+    if(clickEvent) {
+        buttonLeft.addEventListener('click', ()=> {
+            index--;
+            if(index < 0) {
+                index = images.length - 1;
+            }
+            showImage(index);
+        });
+
+        buttonRight.addEventListener('click', ()=> {
+            index++;
+            if(index > images.length - 1) {
+                index = 0;
+            }
+            showImage(index);
+        });
+    }
+
+
+    setInterval(()=> {
+        if(!run){
+            return;
+        }
+    
+        index++;
+        if(index > images.length - 1) {
+              index = 0;
+        }
+        showImage(index)
+    }, time)
+}
+
+export default slideShow;
+
+
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+# An Image Slider
+
+Create an image slider using the slideShow function with five parameters
+
+Usage:
+``` javascript
+import createElement from "https://unpkg.com/@silv21/image_slider@1.0.0/index.js";
+
+// creating an image slider with a **Click Event** (true) for the user to slide back and forth
+// imageElement is provided to have a starting image 
+// slider will automatically change image every 2 sec (2000 ms, arbitrary time settings) if not clicked
+// an array needs to be provided
+// mouse event is off (false)
+var array = ["./img1.jpg", "./img2.jpg", "./img3.jpg", "./img4.jpg"]
+
+slideShow(imageElement, array, 2000, false, true);
+```
+
+``` javascript
+import createElement from "https://unpkg.com/@silv21/image_slider@1.0.0/index.js";
+
+// creating an image slider with **Mouse Event** (true) when the user hovers over the image (freezes the image)
+// imageElement is provided to have a starting image 
+// slider will automatically change image every 2 sec (2000 ms, arbitrary time settings) if not clicked
+// an array needs to be provided
+// click event is off (false)
+var array = ["./img1.jpg", "./img2.jpg", "./img3.jpg", "./img4.jpg"]
+
+slideShow(imageElement, array, 2000, true, false);
